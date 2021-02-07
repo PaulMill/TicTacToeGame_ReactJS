88e85919cc880dabe5938570ae7e9567b81cb64d
Paul Miller <usa.paul@icloud.com>
2021-02-06 23:38:45 -0800

    Set HTML structure

--- src/App.js
+++ src/App.js
@@ -3,8 +3,39 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      Hello game
+    <div className="app">
+      <header className="app-header">
+        <h1>T-T-T Game</h1>
+      </header>
+      <section className="game-area">
+        <ul className="game-wrapper">
+          <li className="box"><p className="sign">X</p></li>
+          <li className="box"><p className="sign">O</p></li>
+          <li className="box">1</li>
+          <li className="box">1</li>
+          <li className="box">1</li>
+          <li className="box">1</li>
+          <li className="box">1</li>
+          <li className="box">1</li>
+          <li className="box">1</li>
+        </ul>
+        <div className="game-controls">
+          <div className="btn">RESET RESULTS</div>
+          <div className="player-result-wrapper">
+            <p className="player">PLAYER (X)</p>
+            <p className="score">0</p>
+          </div>
+          <div className="player-result-wrapper">
+            <p className="player">TIES</p>
+            <p className="score">0</p>
+          </div>
+          <div className="player-result-wrapper">
+            <p className="player">PLAYER (O)</p>
+            <p className="score">0</p>
+          </div>
+          <div className="btn">NEW ROUND</div>
+        </div>
+      </section>
     </div>
   );
 }
