ed56e9f31ec7d8e9c00c6dc20c553ead7aa61271
Paul Miller <usa.paul@icloud.com>
2021-02-07 13:54:34 -0800

    Add css to winning player text box

--- src/UI_Components/PlayerResult.jsx
+++ src/UI_Components/PlayerResult.jsx
@@ -2,10 +2,11 @@ import React from 'react';
 
 export const PlayerResult = ({textPlayer, score, isWon}) => {
     const textClass = `player-result-wrapper ${isWon ? 'player-won' : ''}`;
+    const styleWinScore = isWon ? { textShadow: "0 0 1em #e7ffc4" } : null;
     return (
         <div className={textClass}>
             <p className="player">{textPlayer}</p>
-            <p className="score">{score}</p>
+            <p style={styleWinScore} className="score">{score}</p>
           </div>
     )
 }
