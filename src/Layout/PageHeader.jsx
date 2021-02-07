5af2678da57ce84cc970a0b81bcccbd8c9ead748
Paul Miller <usa.paul@icloud.com>
2021-02-07 14:32:38 -0800

    Add media query for ipad and iphone

--- src/Layout/PageHeader.jsx
+++ src/Layout/PageHeader.jsx
@@ -2,6 +2,6 @@ import React from 'react';
 
 export const PageHeader = () => (
     <header className="app-header">
-        <h1>T-T-T Game</h1>
+        <h1 className="app-header_text">T-T-T Game</h1>
     </header>
 );
