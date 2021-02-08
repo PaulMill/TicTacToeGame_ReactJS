75e77221b02fcbe40b03e82d4f5a7e8f32a6778c
Paul Miller <usa.paul@icloud.com>
2021-02-08 00:02:50 -0800

    utilFunctions test - check function checkIfWon

--- src/utils/utilFunc.test.js
+++ src/utils/utilFunc.test.js
@@ -1,6 +1,36 @@
-import { getChar, checkIfWon, addScore, resetWonScore, checkIfTies } from './';
+import { 
+    GRID_3X3 as initGrid, 
+    WINNING_PLAYER as initScore, 
+    getChar, 
+    checkIfWon, 
+    addScore, 
+    resetWonScore, 
+    checkIfTies  
+} from './';
 
 describe('Utility functions', () => {
+    const mockWinningGrid = [
+        {id: 0, selected: true, selection: "X", isWinning: false},
+        {id: 1, selected: false, selection: "", isWinning: false},
+        {id: 2, selected: false, selection: "", isWinning: false},
+        {id: 3, selected: false, selection: "", isWinning: false},
+        {id: 4, selected: true, selection: "X", isWinning: false},
+        {id: 5, selected: false, selection: "", isWinning: false},
+        {id: 6, selected: false, selection: "", isWinning: false},
+        {id: 7, selected: false, selection: "", isWinning: false},
+        {id: 8, selected: true, selection: "X", isWinning: false},
+    ];
+    const mockTiesGrid = [
+        {id: 0, selected: true, selection: "O", isWinning: false},
+        {id: 1, selected: true, selection: "X", isWinning: false},
+        {id: 2, selected: true, selection: "X", isWinning: false},
+        {id: 3, selected: true, selection: "X", isWinning: false},
+        {id: 4, selected: true, selection: "X", isWinning: false},
+        {id: 5, selected: true, selection: "O", isWinning: false},
+        {id: 6, selected: true, selection: "O", isWinning: false},
+        {id: 7, selected: true, selection: "O", isWinning: false},
+        {id: 8, selected: true, selection: "X", isWinning: false},
+    ];
 
     it('getChar pass odd number return X and even numbers return O', () => {
         expect(getChar(2)).toEqual('X');
@@ -8,9 +38,13 @@ describe('Utility functions', () => {
         expect(getChar("2")).toEqual('X');
         expect(getChar("1")).toEqual('O');
     })
-
     it('check if crashed getChar function if not correct data type value', () => {
         expect(getChar({})).toEqual("O");
+    })
+
+    it('check function checkIfWon pass grids', () => {
+        expect(checkIfWon(mockWinningGrid)).toEqual(['X', [0, 4, 8]]);
+        expect(checkIfWon(mockTiesGrid)).toEqual([null, null]);
     })
     
 })
