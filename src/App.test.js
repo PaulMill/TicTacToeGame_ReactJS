81f5d71816dba15f3f7596106aca28ef533ca6ce
Paul Miller <usa.paul@icloud.com>
2021-02-07 16:53:48 -0800

    App component - test has one page header

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import App from './App';
+import { PageHeader } from './Layout';
 import { shallow } from 'enzyme';
 
 describe('App component', () => {
   it('renders without crash', () => {
     const appWrapper = shallow(<App />)
+  })
+
+  it('renders a page header component', () => {
+    const appWrapper = shallow(<App />);
+    const pageHeaderList = appWrapper.find(PageHeader);
+
+    expect(pageHeaderList).toHaveLength(1);
   })
 });
