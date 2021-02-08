6485d9b0c07b5004398f3b9397dc59a1fafaa57c
Paul Miller <usa.paul@icloud.com>
2021-02-07 19:08:12 -0800

    GameArea component - test render GameControlWrapper component

--- src/Layout/GameArea.test.js
+++ src/Layout/GameArea.test.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import { render, fireEvent, getByTestId} from "@testing-library/react";
-import { GameArea, GameWrapper } from './';
+import { GameArea, GameWrapper, GameControlsWrapper } from './';
 import { WINNING_PLAYER as initScore } from '../utils';
 
 describe('GameArea component', () => {
-    const propsGame = { score: initScore, setScore: jest.fn(), resetGrid: false, setResetGrid: jest.fn(), setRoundEnds: jest.fn() }
     let gameAreaWrapper;
 
     beforeAll(() => {
-        gameAreaWrapper = shallow(<GameArea {...propsGame} />);
+        gameAreaWrapper = shallow(<GameArea />);
     });
 
     it('Should render GameWrapper component', () => {
         const gameWrapperList = gameAreaWrapper.find(GameWrapper);
         expect(gameWrapperList).toBeTruthy();
+    });
+
+    it('Should render GameControlsWrapper component', () => {
+        const gameControlList = gameAreaWrapper.find(GameControlsWrapper);
+        expect(gameControlList).toBeTruthy();
     });
 });
