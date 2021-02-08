e1f3b1ee51de7731f5fb93c9d6f27a68b715973c
Paul Miller <usa.paul@icloud.com>
2021-02-07 20:42:26 -0800

    GameWrap test - set chars X and O on every next click

--- src/Layout/GameWrapper.test.js
+++ src/Layout/GameWrapper.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { shallow, mount } from 'enzyme';
 import { GameWrapper } from './';
+import { Box } from '../UI_Components';
 import { WINNING_PLAYER as initScore } from '../utils';
 
 describe('GameWrapper component', () => {
@@ -17,8 +18,17 @@ describe('GameWrapper component', () => {
     })
 
     it('set by default 9 empty boxes', () => {
-        const boxesList = wrapper.find('li');
+        const boxesList = wrapper.find(Box);
         expect(boxesList).toHaveLength(9);
         expect(boxesList.map(el => el.text())).toEqual(["","","","","","","","",""]);
+    })
+
+    it('set X and O on every next click', () => {
+        const boxesList = wrapper.find(Box);
+        boxesList.at(0).simulate('click');
+        boxesList.at(1).simulate('click');
+        boxesList.at(2).simulate('click');
+        boxesList.at(3).simulate('click');
+        expect(boxesList.map(b => b.text())).toEqual(["X","O","X","O","","","","",""]);
     })
 })
