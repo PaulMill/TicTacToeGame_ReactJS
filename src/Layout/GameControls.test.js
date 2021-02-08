70ea84dce688b3660ef8cb1bd2ad66d2b2dc85c2
Paul Miller <usa.paul@icloud.com>
2021-02-08 01:31:10 -0800

    GameControls test - check score PlayerResults values from props

--- src/Layout/GameControls.test.js
+++ src/Layout/GameControls.test.js
@@ -5,6 +5,11 @@ import { WINNING_PLAYER as initScore } from '../utils';
 
 describe('GameControlsWrapper component', () => {
     const props = { score: initScore, setScore: jest.fn(), setNewRound: jest.fn(), roundEnds: false }
+    const mockNewScore = {
+        X: { score: 0, isWon: false },
+        T: { score: 2, isWon: false },
+        O: { score: 1, isWon: false }
+    }
     
     let wrapper;
 
@@ -25,6 +30,13 @@ describe('GameControlsWrapper component', () => {
     it('check newRound button clicked and call functions', () => {
         wrapper.find('.btn').at(1).simulate('click')
         expect(props.setNewRound).toBeCalled();
+    })
+
+    it('check correctly set PlayerResults values from props', () => {
+        wrapper.setProps({...props, score: mockNewScore})
+        expect(wrapper.find('.score').at(0).text()).toBe("0");
+        expect(wrapper.find('.score').at(1).text()).toBe("2");
+        expect(wrapper.find('.score').at(2).text()).toBe("1");
     })
 
 })
