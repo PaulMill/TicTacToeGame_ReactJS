183700c4d837e4acdfe6671e1c2418cbb2306773
Paul Miller <usa.paul@icloud.com>
2021-02-07 23:19:41 -0800

    Button test - class applied once set isActive

--- src/UI_Components/Button.test.js
+++ src/UI_Components/Button.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import { Button } from './';
 
-describe('PlayerResult component', () => {
+describe('Button component', () => {
     const props = { text: "RESET", clicked: jest.fn(), isActive: false, color: "#ccc" }
     
     let wrapper;
@@ -13,5 +13,10 @@ describe('PlayerResult component', () => {
 
     it('component renders correctly', () => {
         expect(wrapper).not.toBeNull();
+    })
+
+    it('check if class applied once set isActive ', () => {
+        wrapper.setProps({ text: "RESET", clicked: jest.fn(), isActive: true, color: "#ccc" });
+        expect(wrapper.find('.btn-active')).toHaveLength(1);
     })
 })
